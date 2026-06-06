9afa4103e06049f8e0c99fb783f29b751cd4c648
Mspagnolo27248 <b9hrqzwjmk@privaterelay.appleid.com>
2026-06-06 16:04:07 -0400

    rollback to v1

--- src/data/index.js
+++ src/data/index.js
@@ -2,8 +2,9 @@ import solarSystem from './topics/solar-system.json'
 import americanRevolution from './topics/american-revolution.json'
 import marieCurie from './topics/marie-curie.json'
 import cocoChanel from './topics/coco-chanel.json'
+import waltDisney from './topics/walt-disney.json'
 
-export const topics = [solarSystem, americanRevolution, marieCurie, cocoChanel]
+export const topics = [solarSystem, americanRevolution, marieCurie, cocoChanel, waltDisney]
 
 export function getTopicById(id) {
   return topics.find(t => t.id === id) || null
