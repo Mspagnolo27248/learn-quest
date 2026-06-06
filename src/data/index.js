78d0c13c0d6688c36d63d7b1c99287ffb3541844
Mspagnolo27248 <b9hrqzwjmk@privaterelay.appleid.com>
2026-06-06 15:29:01 -0400

    coco chanel

--- src/data/index.js
+++ src/data/index.js
@@ -1,8 +1,9 @@
 import solarSystem from './topics/solar-system.json'
 import americanRevolution from './topics/american-revolution.json'
 import marieCurie from './topics/marie-curie.json'
+import cocoChanel from './topics/coco-chanel.json'
 
-export const topics = [solarSystem, americanRevolution, marieCurie]
+export const topics = [solarSystem, americanRevolution, marieCurie, cocoChanel]
 
 export function getTopicById(id) {
   return topics.find(t => t.id === id) || null
